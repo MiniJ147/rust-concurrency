23f0b8e24d8c4200918fc0498830d4fdabf0e55d
Boop <156966825+boop123-dotcom@users.noreply.github.com>
2025-05-09 15:04:24 -0400

    Update mdlist.rs

--- src/priority_queue/src/mdlist.rs
+++ src/priority_queue/src/mdlist.rs
@@ -1,7 +1,13 @@
-usae crossbeam::epoch::{self, Atomic, Owned, Shared, Guard};
+use crossbeam::epoch::{self, Atomic, Owned, Shared, Guard};
 use std::sync::atomic::Ordering;
 
 pub const DIMENSION: usize = 8;
+
+const ADPINV_MASK: usize = 1;
+const PRGINV_MASK: usize = 2;
+const INVALID_MASK: usize = 3;
+const MARKED_MASK: usize = 1;
+const DELETED_MASK: usize = 1;
 
 pub struct Desc {
     curr: Atomic<Node>,
@@ -32,8 +38,60 @@ pub struct MDList {
     purge: Atomic<Stack>,
 }
 
-const ADPINV_MASK: usize = 1;
-const PRGINV_MASK: usize = 2;
-const INVALID_MASK: usize = 3;
-const MARKED_MASK: usize = 1;
-const DELETED_MASK: usize = 1;
+impl MDList {
+    pub fn set_adpinv(ptr: *mut Node) -> *mut Node {
+        ((ptr as usize) | ADPINV_MASK) as *mut Node
+    }
+    
+    pub fn clr_adpinv(ptr: *mut Node) -> *mut Node {
+        ((ptr as usize) & !ADPINV_MASK) as *mut Node
+    }
+    
+    pub fn is_adpinv(ptr: *mut Node) -> bool {
+        ((ptr as usize) & ADPINV_MASK) != 0
+    }
+    
+    pub fn set_prginv(ptr: *mut Node) -> *mut Node {
+        ((ptr as usize) | PRGINV_MASK) as *mut Node
+    }
+    
+    pub fn clr_prginv(ptr: *mut Node) -> *mut Node {
+        ((ptr as usize) & !PRGINV_MASK) as *mut Node
+    }
+    
+    pub fn is_prginv(ptr: *mut Node) -> bool {
+        ((ptr as usize) & PRGINV_MASK) != 0
+    }
+    
+    pub fn clr_invalid(ptr: *mut Node) -> *mut Node {
+        ((ptr as usize) & !INVALID_MASK) as *mut Node
+    }
+    
+    pub fn is_invalid(ptr: *mut Node) -> bool {
+        ((ptr as usize) & INVALID_MASK) != 0
+    }
+    
+    pub fn set_marked(ptr: *mut Stack) -> *mut Stack {
+        ((ptr as usize) | MARKED_MASK) as *mut Stack
+    }
+    
+    pub fn clr_marked(ptr: *mut Stack) -> *mut Stack {
+        ((ptr as usize) & !MARKED_MASK) as *mut Stack
+    }
+    
+    pub fn is_marked(ptr: *mut Stack) -> bool {
+        ((ptr as usize) & MARKED_MASK) != 0
+    }
+    
+    pub fn set_deleted(ptr: *mut Node) -> *mut Node {
+        ((ptr as usize) | DELETED_MASK) as *mut Node
+    }
+    
+    pub fn clr_deleted(ptr: *mut Node) -> *mut Node {
+        ((ptr as usize) & !DELETED_MASK) as *mut Node
+    }
+    
+    pub fn is_deleted(ptr: *mut Node) -> bool {
+        ((ptr as usize) & DELETED_MASK) != 0
+    }
+}
